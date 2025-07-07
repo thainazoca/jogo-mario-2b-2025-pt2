d44afafb6a97ab1f240f3c53c5f27ba226516926
thainazoca <thaina.barbosa.santos@escola.pr.gov.br>
2025-07-07 19:38:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 const mario = document.querySelector(".mario")
 const pipe = document.querySelector(".pipe")
+const nuvem = document.query.Selector(".nuvem")
+const startButton = document.query.Selector(".start")
+const gameOverScreen = document.querySelector(".game-over")
+const scoreElement = document.querySelector(".score")
+
+audioStart = new Audio("./sound/audio_theme.mp3");
+const gameOverSound = new Audio("./sound/audio_gameover.mp3");
+
+let gameStarted = false;
+let score = 0;
+
+const startGame = () => {
+    gameStarted = true;
+    audioStart.play();
+
+    pipe.style.animation = "pipe-animation 1.5s infinite linear";
+
+    startButton.style.display = "none";
+    mario.style.opacity = "1";
+    pipe.style.opacity = "1";
+    nuvem.style.opacity = "1";
+}
+
 
 const jump = () => {
     mario.classList.add("jump");
@@ -8,6 +31,13 @@ const jump = () => {
         mario.classList.remove("jump");
     }
     ,500);
+}
+
+const updateScore = () => {
+    score += 1;
+    scoreElement.textContent = score;
+    const animationSpeed = 1.5/ (1 + score/500);
+    pipe.style.animation = `pipe-animation ${animationSpeed}s infinie linear`;
 }
 
 const loop = setInterval(() => {
@@ -25,7 +55,19 @@ const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
         mario.style.width = "75px";
         mario.style.marginLeft = "50px";
 
+        audioStart.pause();
+        gameOverSound.play();
+    
         clearInterval(loop);
-    }
+        gameOverScreen.style.display = "flex";
+    }else if (pipePosition < 0 && gameStarted) {
+        uptadeScore();
+        pipe.style.left = ' ';
+       }
 },10);
+
 document.addEventListener("keydown", jump);
+
+const restartGame = () => {
+    window.location.reload();
+}
